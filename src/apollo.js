90e2bb94e84e4ccd6fb2f64f4403983d04755491
Lee Seul Yi <63714074+Seyiul@users.noreply.github.com>
2024-02-07 16:02:47 +0900

    Update apollo.js

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://sucha-movie-ql.herokuapp.com/",
+  uri: "https://movie-ql-seulyi.koyeb.app/",
   cache: new InMemoryCache(),
 });
 
